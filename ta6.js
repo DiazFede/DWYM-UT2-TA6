f02d20e30f88524c17d50d8726e777e21aebb109
Federico Diaz <131992983+DiazFede@users.noreply.github.com>
2024-08-19 15:00:25 -0300

    Add files via upload

---
+++ ta6.js
@@ -0,0 +1,10 @@
+function convertToCelsius(temp) {
+    console.log((temp - 32) * 5 / 9);
+}
+
+function convertToFahrenheit(temp) {
+    console.log((temp * 9 / 5) + 32);
+}
+
+convertToCelsius(32);
+convertToFahrenheit(0);
